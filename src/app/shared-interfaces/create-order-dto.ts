771e17b693de6d5fe8d3f92173aed6f358f1ff04
ike-Njoku <ikenjokudc@gmail.com>
2021-12-08 17:36:57 +0100

    updates

---
+++ src/app/shared-interfaces/create-order-dto.ts
@@ -0,0 +1,18 @@
+import { GetProductDto } from "../shared-services/get-product-dto";
+export interface  DeliveryAdddress {
+  firstName: string,
+  lastName: string,
+  emailAddress: string,
+  country: string,
+  state: string,
+  city: string,
+  primaryPhoneNumber: string,
+  secondaryPhoneNumber?: string,
+  address: string
+};
+
+export interface CreateOrderDTO {
+  transactionReference: string;
+  deliveryAddress: DeliveryAdddress;
+  products: GetProductDto[]
+}
