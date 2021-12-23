33e4c920fd76ec7223e4d7ffeb0ac1abb97a4059
ike-Njoku <ikenjokudc@gmail.com>
2021-12-23 07:38:46 +0100

    updates

--- src/app/admin/dashboard/dashboard-routing.module.ts
+++ src/app/admin/dashboard/dashboard-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent , children: [
-    { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
-  ]},
+  {
+    path: '', component: DashboardComponent, children: [
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+      { path: 'products/list', loadChildren: () => import('./product-list/product-list.module').then(m => m.ProductListModule) },
+    ]
+  },
 ];
 
 @NgModule({
---
+++ src/app/admin/dashboard/product-list/product-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { PopUpNotificationService } from 'src/app/pop-up-notification/pop-up-notification.service';
+import { ServerResponseDto } from 'src/app/shared-interfaces/server-response-dto';
+import { GetProductDto } from 'src/app/shared-services/get-product-dto';
+import { ProductsService } from 'src/app/shared-services/products.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  allProducts: GetProductDto[] = [];
+  selectedProduct!: GetProductDto;
+  public readonly apiBase: string = environment.apiBaseUrl;
+
+  constructor(
+    private productService: ProductsService,
+    private popUpService: PopUpNotificationService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllProducts()
+  }
+
+  getAllProducts() {
+    this.productService.getAllProducts()
+      .subscribe(
+        (response: ServerResponseDto) => this.allProducts = response.data,
+        (error: any) => this.popUpService.addNotification(error, 5000)
+      )
+  }
+
+  toggleModal(modalId: string) {
+    document.getElementById(modalId)?.classList.toggle('active');
+    document.getElementById('main')?.classList.toggle('blur')
+  }
+
+  selectProduct(product: GetProductDto) {
+    this.selectedProduct = product;
+  }
+
+  toggleUpdateModal() {
+    document.getElementById('update-modal')
+  }
+
+  deleteProduct(product: GetProductDto) {
+    this.productService.deleteProduct(product)
+      .subscribe(
+        (response: ServerResponseDto) => {
+          console.log(response);
+          this.popUpService.addNotification(response.message, 5000);
+          if (response.status == 'success') {
+            this.toggleModal('delete-modal');
+            this.allProducts.splice(this.allProducts.indexOf(product), 1);
+          }
+        },
+        (error: string) => this.popUpService.addNotification(error, 5000)
+      )
+  }
+}
