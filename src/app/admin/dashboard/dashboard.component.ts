a8185348f3a8758ddde24bbc4614f089c491a8b2
ike-njoku <ikenjokudc@gmail.com>
2022-01-20 23:02:58 +0100

    Updates

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  signOut() {
+    localStorage.removeItem(this.authService.sessionStorageString);
+    return this.router.navigate(['/admin/sign-in']);
   }
 
 }
