a62ef26a98a193cf81aab5406f010fac22539b78
ike-njoku <ikenjokudc@gmail.com>
2022-04-07 22:32:00 +0100

    Updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'https://stella-art-store-backend.herokuapp.com',
+  apiBaseUrl: 'https://s-maris-artstore-backend.herokuapp.com',
   authTokenUrl: 'https://api-m.paypal.com/v1/oauth2/token',
   payUrl: 'https://api-m.paypal.com/v2/checkout/orders',
   paypalParams: 'QVptcXFlMDRVMHctMktXWEtEVS1qQkMzUW1YTnlrOUVyVDM1MXJOdFg0a0Uxamtua19RZDFKV1QzVUwtMmt6YTU1eFkyZlZ6bHFiazFLeG06RUJ0QkR2RkFYWk5MX29HQXNoWmpVTUZOcU81V3ZpTjhQUzdRcWtJZEFqQng1bHY3VGZvaVVGSHNMMjlONDQ4dzV0ZUY5SWNPMXdQZzNtOG8=',
